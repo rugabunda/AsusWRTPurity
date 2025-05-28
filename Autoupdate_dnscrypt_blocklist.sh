c1bd67a0d0b5509329927db674431074aceff8e6
titus <38260267+rugabunda@users.noreply.github.com>
2025-05-28 14:21:09 -0700

    Update Autoupdate_dnscrypt_blocklist.sh

--- Autoupdate_dnscrypt_blocklist.sh
+++ Autoupdate_dnscrypt_blocklist.sh
@@ -24,7 +24,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Delete everything after line 345 and append downloaded content
+# Delete everything in blocklist.txt after line 345 and append downloaded content
 sed -i '346,$d' "$BLOCKLIST"
 cat "$DOWNLOADED_FILE" >> "$BLOCKLIST"
 
